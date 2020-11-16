ce7c86d65fb084a6e96346c833f86e3e8c0ca8b8
linuxmint20 <linuxmint20@linuxmint20-virtual-machine>
2020-11-16 22:16:05 +0100

    unificacion_rama_cambio_con_master.

--- src/anton/geyko/geyko/adivino/Aciertos.java
+++ src/anton/geyko/geyko/adivino/Aciertos.java
@@ -5,7 +5,7 @@ public class Aciertos {
     public static void main(String[] args) {
         int n = 0,cont = 0;
         int x = (int) (Math.random() * (10 - 50)) + 50;
-        //System.out.print(x);
+        //System.out.print(x); 
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("\nIntroduce el valor de X: ");
